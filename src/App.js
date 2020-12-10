d558c92c791e56d1bf33899baa06a6f57a22ed96
anselbrandt <ansel@anselbrandt.com>
2020-12-10 14:14:19 -0500

    Show all charts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-// import MultilineChart from "./MultilineChart";
+import MultilineChart from "./MultilineChart";
 import SvgChart from "./SvgChart";
-// import CanvasChart from "./CanvasChart";
+import CanvasChart from "./CanvasChart";
 
 function App() {
   return (
@@ -11,11 +11,15 @@ function App() {
       <div className="SubTitle" style={{ marginBottom: 30 }}>
         SVG, Canvas and Multiline
       </div>
-      <div style={{ margin: 30 }}>{/* <MultilineChart /> */}</div>
+      <div style={{ margin: 30 }}>
+        <MultilineChart />
+      </div>
       <div style={{ margin: 30 }}>
         <SvgChart />
       </div>
-      <div style={{ margin: 30 }}>{/* <CanvasChart /> */}</div>
+      <div style={{ margin: 30 }}>
+        <CanvasChart />
+      </div>
     </div>
   );
 }
